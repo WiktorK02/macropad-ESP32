9f6a32867cf790176c0518e60c88c1402c3ebf6f
WiktorK02 <wiktor.kidon@hotmail.com>
2023-12-08 18:44:53 +0100

    updated: proper saving to json file, stabilization of wifi handling

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-
-
 #include <BfButton.h>
 #include <Bounce2.h>
 #include <WiFiManager.h>
@@ -39,27 +37,33 @@ const char* mqtt_switch1_topic = "switch/click/1";
 const char* mqtt_switch2_topic = "switch/click/2";
 const unsigned long RECONNECT_DELAY = 5000;
 
-WiFiManagerParameter mqttServer("mqtt_server", "MQTT Server", "192.168.0.23", 40);
-WiFiManagerParameter mqttPort("mqtt_port", "MQTT Port", "1883", 6);
-WiFiManagerParameter mqttUser("mqtt_user", "MQTT User", "admin", 20);
-WiFiManagerParameter mqttPassword("mqtt_password", "MQTT Password", "admin", 20);
+WiFiManagerParameter mqttServer("mqtt_server", "MQTT Server", "", 40);
+WiFiManagerParameter mqttPort("mqtt_port", "MQTT Port", "", 6);
+WiFiManagerParameter mqttUser("mqtt_user", "MQTT User", "", 20);
+WiFiManagerParameter mqttPassword("mqtt_password", "MQTT Password", "", 20);
 void saveSettingsToJson() {
   DynamicJsonDocument doc(1024);
 
-  doc["mqtt_server"] = mqttServer.getValue();
-  doc["mqtt_port"] = mqttPort.getValue();
-  doc["mqtt_user"] = mqttUser.getValue();
-  doc["mqtt_password"] = mqttPassword.getValue();
+  if (strlen(mqttServer.getValue()) > 0 &&
+      strlen(mqttPort.getValue()) > 0 &&
+      strlen(mqttUser.getValue()) > 0 &&
+      strlen(mqttPassword.getValue()) > 0) {
+    doc["mqtt_server"] = mqttServer.getValue();
+    doc["mqtt_port"] = mqttPort.getValue();
+    doc["mqtt_user"] = mqttUser.getValue();
+    doc["mqtt_password"] = mqttPassword.getValue();
 
-  File configFile = SPIFFS.open(settingsFile, "w");
-  if (!configFile) {
-    Serial.println("Failed to open config file for writing");
-    return;
+    File configFile = SPIFFS.open(settingsFile, "w");
+    if (configFile) {
+      serializeJson(doc, configFile);
+      configFile.close();
+      Serial.println("Settings saved to config.json");
+    } else {
+      Serial.println("Failed to open config file for writing");
+    }
+  } else {
+    Serial.println("MQTT settings are empty. Not saving to config.json");
   }
-
-  serializeJson(doc, configFile);
-  configFile.close();
-  Serial.println("Settings saved to config.json");
 }
 
 void loadSettingsFromJson() {
@@ -69,30 +73,36 @@ void loadSettingsFromJson() {
   }
 
   File configFile = SPIFFS.open(settingsFile, "r");
-  if (!configFile) {
+  if (configFile) {
+    DynamicJsonDocument doc(1024);
+    DeserializationError error = deserializeJson(doc, configFile);
+    configFile.close();
+
+    if (!error) {
+      String mqttServerValue = doc["mqtt_server"].as<String>();
+      String mqttPortValue = doc["mqtt_port"].as<String>();
+      String mqttUserValue = doc["mqtt_user"].as<String>();
+      String mqttPasswordValue = doc["mqtt_password"].as<String>();
+
+      if (mqttServerValue.length() > 0 &&
+          mqttPortValue.length() > 0 &&
+          mqttUserValue.length() > 0 &&
+          mqttPasswordValue.length() > 0) {
+        mqttServer.setValue(mqttServerValue.c_str(), mqttServerValue.length());
+        mqttPort.setValue(mqttPortValue.c_str(), mqttPortValue.length());
+        mqttUser.setValue(mqttUserValue.c_str(), mqttUserValue.length());
+        mqttPassword.setValue(mqttPasswordValue.c_str(), mqttPasswordValue.length());
+
+        Serial.println("Settings loaded from config.json");
+      } else {
+        Serial.println("Loaded MQTT settings are empty. Using default values.");
+      }
+    } else {
+      Serial.println("Failed to parse config file");
+    }
+  } else {
     Serial.println("Failed to open config file for reading");
-    return;
   }
-
-  DynamicJsonDocument doc(1024);
-  DeserializationError error = deserializeJson(doc, configFile);
-  if (error) {
-    Serial.println("Failed to parse config file");
-    return;
-  }
-
-  String mqttServerValue = doc["mqtt_server"].as<String>();
-  String mqttPortValue = doc["mqtt_port"].as<String>();
-  String mqttUserValue = doc["mqtt_user"].as<String>();
-  String mqttPasswordValue = doc["mqtt_password"].as<String>();
-
-  mqttServer.setValue(mqttServerValue.c_str(), mqttServerValue.length());
-  mqttPort.setValue(mqttPortValue.c_str(), mqttPortValue.length());
-  mqttUser.setValue(mqttUserValue.c_str(), mqttUserValue.length());
-  mqttPassword.setValue(mqttPasswordValue.c_str(), mqttPasswordValue.length());
-
-  configFile.close();
-  Serial.println("Settings loaded from config.json");
 }
 
 void connectToMQTT() {
@@ -132,6 +142,11 @@ void pressHandler(BfButton *btn, BfButton::press_pattern_t pattern) {
 }
 
 void setup() {
+  if (!SPIFFS.begin(true)) {
+    Serial.println("Failed to mount file system");
+
+    return;
+  }
   Serial.println(angle);
   pinMode(CLK, INPUT_PULLUP);
   pinMode(DT, INPUT_PULLUP);
@@ -141,7 +156,7 @@ void setup() {
       .onDoublePress(pressHandler) 
       .onPressFor(pressHandler, 1000); 
 
-  //wm.resetSettings();
+  // wm.resetSettings();
   pinMode(switchPin, INPUT_PULLUP); 
   debouncer.attach(switchPin);
   pinMode(switchPin2, INPUT_PULLUP); 
@@ -150,39 +165,52 @@ void setup() {
   debouncer.interval(50); 
   Serial.begin(115200);
 
-    wm.addParameter(&mqttServer);
-    wm.addParameter(&mqttPort);
-    wm.addParameter(&mqttUser);
-    wm.addParameter(&mqttPassword);
+  wm.addParameter(&mqttServer);
+  wm.addParameter(&mqttPort);
+  wm.addParameter(&mqttUser);
+  wm.addParameter(&mqttPassword);
+  wm.setMinimumSignalQuality();
 
-    bool res = wm.autoConnect("MacroPad", "password");
-    if (!res) {
-        Serial.println("Failed to connect");
-    } else {
-        Serial.println("Connected to WiFi");
+  wm.setConnectTimeout(180);
+
+  wm.setTimeout(180);
+
+    //if (digitalRead(btnPin) == LOW) {
+    //Serial.println("Resetting WiFi settings...");
+    //wm.resetSettings();
+  //}
+
+  // Attempt to autoconnect to WiFi
+  bool res = wm.autoConnect("MacroPad", "password");
+
+  if (!res) {
+    Serial.println("Failed to autoconnect. Retrying...");
+
+  } else {
+    Serial.println("Connected to WiFi");
+    saveSettingsToJson();
+  }
+  loadSettingsFromJson();
+  delay(1000);
+  connectToMQTT();
+
+  if (!mqttServer.getValue() || !mqttPort.getValue() || !mqttUser.getValue() || !mqttPassword.getValue()) {
+    Serial.println("MQTT settings are missing. Please configure them through the captive portal.");
+  } else {
+
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
     }
 
-    delay(1000);
-    connectToMQTT();
-
-    loadSettingsFromJson();
-
-    if (!mqttServer.getValue() || !mqttPort.getValue() || !mqttUser.getValue() || !mqttPassword.getValue()) {
-        Serial.println("MQTT settings are missing. Please configure them through the captive portal.");
-    } else {
-
-        while (WiFi.status() != WL_CONNECTED) {
-            delay(500);
-            Serial.print(".");
-        }
-
-        if (!mqttClient.connected()) {
-            Serial.println("MQTT not connected. Attempting to reconnect...");
-            delay(RECONNECT_DELAY); 
-            connectToMQTT();
-        }
+    if (!mqttClient.connected()) {
+      Serial.println("MQTT not connected. Attempting to reconnect...");
+      delay(RECONNECT_DELAY); 
+      connectToMQTT();
     }
+  }
 }
+
 void loop() {
 
     debouncer.update();
